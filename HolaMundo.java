487e243cc2b30c335cb43ed6ec96ba3f0933b2de
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-05-10 11:42:08 -0500

    se imprime mi nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo{
     public static void main(String[] args){
         System.out.println("Hola Mundo");
         System.out.println("Bienvenido a Estructuras de Datos y Algoritmos");
+        System.out.println("Anthony Marcos Moroccoire Pacompia");
     }
 }
